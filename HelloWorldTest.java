bee47150e1d70c85ecb5eb4467f1ddad43de0078
18130165 <62516617+18130165@users.noreply.github.com>
2020-04-12 15:43:53 +0500

    Laba2

---
+++ HelloWorldTest.java
@@ -0,0 +1,17 @@
+package laba2;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+	assertEquals("Проверяем номер зачетки", 18130165, 18130165);
+	assertEquals("Проверяем номер зачетки", 18130154, 18130154);
+	assertEquals("Проверяем номер зачетки", 18130146, 18130146);
+	assertEquals("Проверяем номер зачетки", 18130675, 18130675);
+	assertEquals("Проверяем номер зачетки", 18130017, 18130017);
+	}
+}
